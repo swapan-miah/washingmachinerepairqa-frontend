59acdb7ecd7d0f08419177a68643ae67eb2d60b4
Ismail Miah New Github Account <133479364+swapan-miah@users.noreply.github.com>
2025-05-31 22:23:53 +0600

    this commit by shuvo sarkar

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -2,8 +2,9 @@ import { Toaster } from "react-hot-toast";
 import Header from "@/components/Dashboard/Header";
 import Sidebar from "@/components/Dashboard/Sidebar";
 import SettingsData from "../../../lib/SettingsData";
-import ProtectedRoute from "@/components/ProtectedRoute";
 import HomeData from "../../../lib/HomeData";
+import ProtectedWrapper from "@/components/ProtectedWrapper";
+
 export async function generateMetadata() {
 	const data = await HomeData();
 	return {
@@ -20,20 +21,19 @@ export async function generateMetadata() {
 
 export default async function DashboardLayout({ children }) {
 	const data = await SettingsData();
+
 	return (
-			<div className="overflow-visible">
-				<Toaster
-					position="top-center"
-					reverseOrder={false}
-					containerStyle={{
-						top: 80,
-					}}
-				/>
-				<Header data={data} />
-				<main className="ps-3 flex bg-white">
-					<Sidebar />
-					<div className="flex-1 bg-gray-50">{children}</div>
-				</main>
-			</div>
+		<ProtectedWrapper>
+			<Toaster
+				position="top-center"
+				reverseOrder={false}
+				containerStyle={{ top: 80 }}
+			/>
+			<Header data={data} />
+			<main className="ps-3 flex bg-white">
+				<Sidebar />
+				<div className="flex-1 bg-gray-50">{children}</div>
+			</main>
+		</ProtectedWrapper>
 	);
 }
