39a0353d73a9df19fc9da033bd796a3602a72dba
Ismail Miah New Github Account <133479364+swapan-miah@users.noreply.github.com>
2025-05-31 21:29:17 +0600

    this commit by shuvo sarkar

--- src/app/(dashboard)/dashboard/feedback/edit/[id]/page.js
+++ src/app/(dashboard)/dashboard/feedback/edit/[id]/page.js
@@ -1,10 +1,7 @@
+import axios from "axios";
 import EditFeedBack from "@/components/Dashboard/EditFeedBack";
 
 export default async function page({ params }) {
-	const { id } = await params;
-	const result = await fetch(`${process.env.BASE_URL}/feedback/${id}`, {
-		cache: "no-store", // ensure fresh data
-	});
-	const data = await result.json();
+	const { data } = await axios.get(`${process.env.BASE_URL}/feedback/${params.id}`);
 	return <EditFeedBack data={data} />;
 }
