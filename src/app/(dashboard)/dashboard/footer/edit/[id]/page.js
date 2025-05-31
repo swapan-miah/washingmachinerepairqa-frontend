762655fb151cddbc30dfe36dcf3d7fa46198bc94
Ismail Miah New Github Account <133479364+swapan-miah@users.noreply.github.com>
2025-05-31 21:30:10 +0600

    this commit by shuvo sarkar

--- src/app/(dashboard)/dashboard/footer/edit/[id]/page.js
+++ src/app/(dashboard)/dashboard/footer/edit/[id]/page.js
@@ -1,8 +1,8 @@
+import axios from "axios";
 import EditFooter from "@/components/Dashboard/EditFooter";
 
 export default async function page({ params }) {
-	const { id } = await params;
-	const result = await fetch(`${process.env.BASE_URL}/footer-content/${id}`);
-	const data = await result.json();
+	const { data } = await axios.get(`${process.env.BASE_URL}/footer-content/${params.id}`);
 	return <EditFooter data={data} />;
 }
+
