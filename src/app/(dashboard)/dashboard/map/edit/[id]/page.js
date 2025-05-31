f0d923b1687b7809cf3a02cd02366f64adfadd7a
Ismail Miah New Github Account <133479364+swapan-miah@users.noreply.github.com>
2025-05-31 21:31:12 +0600

    this commit by shuvo sarkar

--- src/app/(dashboard)/dashboard/map/edit/[id]/page.js
+++ src/app/(dashboard)/dashboard/map/edit/[id]/page.js
@@ -1,10 +1,8 @@
+import axios from "axios";
 import EditMaps from "@/components/Dashboard/EditMaps";
 
 export default async function page({ params }) {
-	const { id } = await params;
-	const result = await fetch(`${process.env.BASE_URL}/google-maps/${id}`, {
-		cache: "no-store", // ensure fresh data
-	});
-	const data = await result.json();
+	const { data } = await axios.get(`${process.env.BASE_URL}/google-maps/${params.id}`);
 	return <EditMaps data={data} />;
 }
+
